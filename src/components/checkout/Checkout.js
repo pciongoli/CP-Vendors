1fffaa2cea4c66e35b82008dd44964040c89c1c3
pcion <patrick.ciongoli@gmail.com>
2023-05-04 10:26:17 -0700

    updated checkout

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -12,6 +12,11 @@ const Checkout = () => {
    const { cart } = useContext(CartContext);
    const [name, setName] = useState("");
    const [address, setAddress] = useState("");
+   const [city, setCity] = useState("");
+   const [province, setProvince] = useState("");
+   const [zip, setZip] = useState("");
+   const [country, setCountry] = useState("");
+   const [phone, setPhone] = useState("");
    const [email, setEmail] = useState("");
 
    const handleSubmit = async (e) => {
@@ -32,6 +37,11 @@ const Checkout = () => {
       window.location.href = checkout.webUrl;
    };
 
+   const totalPrice = cart.reduce(
+      (total, item) => total + parseFloat(item.variant.price),
+      0
+   );
+
    return (
       <div className="checkout-container">
          <div>
@@ -43,10 +53,11 @@ const Checkout = () => {
                >
                   <img src={item.variant.image} alt={item.name} />
                   <h2>{item.name}</h2>
-                  <p>Price: ${(item.variant.price / 100).toFixed(2)}</p>
+                  <p>Price: ${parseFloat(item.variant.price).toFixed(2)}</p>
                   <p>Variant: {item.variant.name}</p>
                </div>
             ))}
+            <h2>Total: ${totalPrice.toFixed(2)}</h2>
             <form onSubmit={handleSubmit}>
                <input
                   type="text"
@@ -62,12 +73,47 @@ const Checkout = () => {
                   onChange={(e) => setEmail(e.target.value)}
                   required
                />
+               <input
+                  type="text"
+                  placeholder="Phone Number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  required
+               />
                <textarea
                   placeholder="Shipping Address"
                   value={address}
                   onChange={(e) => setAddress(e.target.value)}
                   required
                ></textarea>
+               <input
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  required
+               />
+               <input
+                  type="text"
+                  placeholder="Province/State"
+                  value={province}
+                  onChange={(e) => setProvince(e.target.value)}
+                  required
+               />
+               <input
+                  type="text"
+                  placeholder="Postal/ZIP Code"
+                  value={zip}
+                  onChange={(e) => setZip(e.target.value)}
+                  required
+               />
+               <input
+                  type="text"
+                  placeholder="Country"
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                  required
+               />
                <button type="submit">Proceed to Checkout</button>
             </form>
          </div>
